a62440297e42ff7c65d8f5a66a87b4b67d751406
mbukacek <35059267+mbukacek@users.noreply.github.com>
2022-07-19 13:27:00 +0200

    Dataload finished

---
+++ scribes_skript_run.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+print("Dataload started started.")
+
+df_main = pd.read_csv('2020-10-15_scribes__data_mf_cleaned.csv', sep = ',', engine = 'python')
+
+print("Source data loaded.")
+
+
+
+
+
+
+
+
+
